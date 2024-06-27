48508f25fcf1878f9da3a4244b866396ea97faf2
Dmitriy <Batunin>
2024-06-27 21:52:52 +0700

    second commit

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ function init() {
     startButton.addEventListener('click', game.start);
     pauseButton.addEventListener('click', game.pause);
     stopButton.addEventListener('click', game.stop);
+
+    window.addEventListener('keydown', game.move);
 }
 
 const GAME_STATUS_STARTED = 'started';
 const GAME_STATUS_PAUSED = 'paused';
 const GAME_STATUS_STOPPED = 'stopped';
+
+const SNAKE_DIRECTION_UP = 'up';
+const SNAKE_DIRECTION_DOWN = 'down';
+const SNAKE_DIRECTION_LEFT = 'left';
+const SNAKE_DIRECTION_RIGHT = 'right';
 
 const config = {
     size: 20
@@ -26,6 +33,7 @@ const game = {
         game.setGameStatus(GAME_STATUS_STARTED);
         board.render();
         snake.render();
+        food.render();
     },
 
     pause() {
@@ -36,6 +44,32 @@ const game = {
         game.setGameStatus(GAME_STATUS_STOPPED);
     },
 
+    move(event){
+        let direction = null;
+
+        switch(event.keyCode){
+            case 38:
+                direction = SNAKE_DIRECTION_UP;
+                break;
+            case 40:
+                direction = SNAKE_DIRECTION_DOWN;
+                break;
+            case 37:
+                direction = SNAKE_DIRECTION_LEFT;
+                break;
+            case 39:
+                direction = SNAKE_DIRECTION_RIGHT;
+                break;
+            default:
+                return;
+        }
+
+        snake.setDirection(direction);
+        const nextPosition = snake.getNextPosition();
+        snake.setPosition(nextPosition);
+        snake.render();
+    },
+
     setGameStatus(status) {
         const element = game.getElement();
         element.classList.remove(GAME_STATUS_STARTED, GAME_STATUS_PAUSED, GAME_STATUS_STOPPED)
@@ -43,6 +77,25 @@ const game = {
     }
 };
 
+const cell = {
+    getElements() {
+        return document.getElementsByClassName('cell')
+    },
+
+    renderClass(coordinates, className) {
+        const cells = this.getElements();
+
+        for (let cell of cells) {
+            cell.classList.remove(className);
+        }
+
+        for (let coordinate of coordinates) {
+            const cell = document.querySelector(`.cell[data-top="${coordinate.top}"][data-left="${coordinate.left}"]`);
+            cell.classList.add(className);
+        }
+    }
+
+}
 const board = {
     getElement() {
         return document.getElementById('board');
@@ -63,38 +116,81 @@ const board = {
 
 const snake = {
 
+    direction: SNAKE_DIRECTION_RIGHT,
+
     parts: [
         { top: 0, left: 0 },
         { top: 0, left: 1 },
         { top: 0, left: 2 },
     ],
 
-    getCellElements() {
-        return document.getElementsByClassName('cell')
+    setDirection(direction){
+        if(this.direction === SNAKE_DIRECTION_DOWN && direction === SNAKE_DIRECTION_UP
+            || this.direction === SNAKE_DIRECTION_UP && direction === SNAKE_DIRECTION_DOWN
+            || this.direction === SNAKE_DIRECTION_RIGHT && direction === SNAKE_DIRECTION_LEFT
+            || this.direction === SNAKE_DIRECTION_LEFT && direction === SNAKE_DIRECTION_RIGHT){
+                return;
+            }
+
+        this.direction = direction;
+    },
+
+    getNextPosition(){
+        const position = {... this.parts[this.parts.length-1]};
+
+        switch(this.direction){
+            case SNAKE_DIRECTION_UP:
+                position.top -= 1;
+                break;
+            case SNAKE_DIRECTION_DOWN:
+                position.top += 1;
+                break;
+            case SNAKE_DIRECTION_LEFT:
+                position.left -= 1;
+                break;
+            case SNAKE_DIRECTION_RIGHT:
+                position.left += 1;
+                break
+        }
+
+        if(position.top === -1){
+            position.top = config.size - 1;
+        }
+        else if(position.top > config.size - 1){
+            position.top = 0;
+        }
+
+        if(position.left === -1){
+            position.left = config.size - 1;
+        }
+        else if(position.left > config.size - 1){
+            position.left = 0;
+        }
+
+        return position;
+    },
+
+    setPosition(position, shift = true){
+        if(shift){
+            this.parts.shift();
+        }
+        this.parts.push(position);
     },
 
     render() {
-        const cells = this.getCellElements();
-
-        for (let cell of cells) {
-            cell.classList.remove('snake');
-        }
-
-        for (let part of this.parts) {
-            const cell = document.querySelector(`.cell[data-top="${part.top}"][data-left="${part.left}"]`);
-            cell.classList.add('snake');
-        }
+        cell.renderClass(this.parts, 'snake');
     }
 
 };
 
 const food = {
+    items:[
+        {top: 5, left: 5}
+    ],
 
+    render(){
+        cell.renderClass(this.items, 'food');
+    }
 };
-
-
-
-
-
 
 window.addEventListener('load', init)
